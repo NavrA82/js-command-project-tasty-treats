9450ec546ec8046500a16138d007867c4cff7b7a
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-07-27 12:17:38 +0300

    new-3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './js/recipe';
+// import './js/recipe';
 import './js/switcher';
-import './js/scroll-up';
+// import './js/scroll-up';
 import './js/slider';
 import './js/header-burger';
-import './js/extrafilters';
+// import './js/extrafilters';
 import './js/theme-switcher';
 // import './js/home_categories-api';
 // import './js/create-order.js';
--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,13 +1,44 @@
-const localStorageRecipes = localStorage.getItem("recipes");
+// const localStorageRecipes = localStorage.getItem("recipes");
 
-if (localStorageRecipes) {
-  const recipeList = document.querySelector(".recipe-list");
+// if (localStorageRecipes) {
+//   const recipeList = document.querySelector(".recipe-list");
 
-  localStorageRecipes.forEach(recipe => {
+//   const recipes = JSON.parse(localStorageRecipes);
+//   recipes.forEach(recipe => {
+//     const recipeItem = document.createElement("li");
+//     recipeItem.textContent = recipe.title;
+//     recipeList.appendChild(recipeItem);
+//   });
+// } else {
+//   document.querySelector(".favorites").textContent = "No favorites found.";
+// }
+
+// const localStorageRecipes = localStorage.getItem("recipes");
+
+// if (localStorageRecipes) {
+//   const recipeList = document.querySelector(".recipe-list");
+
+//   const recipes = JSON.parse(localStorageRecipes);
+//   recipes.forEach(recipe => {
+//     const recipeItem = document.createElement("li");
+//     recipeItem.textContent = recipe.title;
+//     recipeList.appendChild(recipeItem);
+//   });
+// } else {
+//   document.querySelector(".favorites").textContent = "No favorites found.";
+// }
+
+function loadFavorites() {
+  const recipes = localStorage.getItem("recipes");
+  if (!recipes) {
+    document.getElementById("favorites").innerHTML = "No favorites found.";
+    return;
+  }
+  const recipeList = document.getElementById("recipe-list");
+  for (const i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
     const recipeItem = document.createElement("li");
     recipeItem.textContent = recipe.title;
     recipeList.appendChild(recipeItem);
-  });
-} else {
-  document.querySelector(".favorites").textContent = "No favorites found.";
+  }
 }
